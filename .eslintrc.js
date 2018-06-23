ab56a35add848d4d7fdf392966656b10b9721299
Zach Freedman <zfreedman92@gmail.com>
2018-06-23 19:03:24 -0400

    feat: Added ESLint to project

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "extends": "google",
+    "parserOptions": {
+      "ecmaVersion": 2016,
+      "sourceType": "module"
+    },
+    "rules": {
+      "guard-for-in": 0,
+      "no-invalid-this": 0,
+      "no-new-object": 0,
+      "quotes": [2, "backtick"],
+      "require-jsdoc": 0,
+      "spaced-comment": [2, "never"]
+    }
+};
